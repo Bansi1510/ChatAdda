0513e881f294991ca17994bc5ee9af0ad41858ea
prajapati bansi <prajapatibansi1510@gmail.com>
2026-04-30 22:28:45 +0530

    socket.io setup

--- server/src/models/status.model.ts
+++ server/src/models/status.model.ts
@@ -1,12 +1,42 @@
 import mongoose from "mongoose";
+import { ref } from "node:process";
 
 export interface IStatus {
-
+  user: mongoose.Types.ObjectId,
+  content: string,
+  contentType: string,
+  viewers: mongoose.Types.ObjectId[],
+  expiresAt: Date,
+  createdAt: Date,
+  updatedAt: Date,
 }
 
 
 const statusSchema = new mongoose.Schema<IStatus>({
-
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  contentType: {
+    type: String,
+    enum: ["image", "video", "text"],
+    default: 'text'
+  },
+  viewers: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
+    }
+  ],
+  expiresAt: {
+    type: Date,
+    required: true
+  }
 }, { timestamps: true });
 
 const Status = mongoose.model('Status', statusSchema);
